3863bb467a3eb964d26ab122592b0bf6f37d466b
Paul Reioux <reioux@gmail.com>
2014-01-09 01:22:04 -0800

    ARM: be strict about FP exceptions in kernel mode
    The support code in vfp_support_entry does not care whether the
    exception that caused it to be invoked occurred in kernel mode or
    in user mode. However, neither condition that could trigger this
    exception (lazy restore and VFP bounce to support code) is
    currently allowable in kernel mode.
    
    In either case, print a message describing the condition before
    letting the undefined instruction handler run its course and trigger
    an oops.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel at linaro.org>
    Acked-by: Nicolas Pitre <nico at linaro.org>
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	arch/arm/vfp/vfpmodule.c
    
    Former-commit-id: 3d6eae386cf26dd546bbed5de0709b9810b48744

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 12 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Mon Apr 7 18:07:29 CDT 2014"
+#define UTS_VERSION "#12 SMP PREEMPT Mon Apr 7 18:21:10 CDT 2014"
 #define LINUX_COMPILE_BY "blackburn"
 #define LINUX_COMPILE_HOST "blackburn-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g4c7ddff-dirty"
+#define UTS_RELEASE "3.4.0-ga11a9eb-dirty"
