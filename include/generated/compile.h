db1f43183542a12b5aa3eec79ae60d344b0e4684
Blackburn <blackburn2929@gmail.com>
2014-03-17 18:57:50 -0500

    Add in BFQv5r1 scheduler
    Conflicts:
    	block/Kconfig.iosched
    	block/bfq-cgroup.c
    	block/bfq-iosched.c
    	block/bfq-sched.c
    	block/bfq.h
    	include/linux/cgroup_subsys.h
    
    Former-commit-id: 53399d24aeabeb940cffb0cc7891f882842e558c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 7 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sun Apr 6 20:58:06 CDT 2014"
+#define UTS_VERSION "#7 SMP PREEMPT Sun Apr 6 21:36:18 CDT 2014"
 #define LINUX_COMPILE_BY "blackburn"
 #define LINUX_COMPILE_HOST "blackburn-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-gf9a3861-dirty"
+#define UTS_RELEASE "3.4.0-g6ef56ad-dirty"
