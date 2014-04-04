528cbfb99ac1bee9ee8c7da204447fadd6e09d0e
flar2 <asegaert@gmail.com>
2014-04-04 02:46:13 -0400

    Add pronto wlan driver
    Conflicts:
    	drivers/staging/Kconfig
    
    Former-commit-id: 4646eb693672988f25aa111a31f8347d789002c0

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 14 */
+/* This file is auto generated, version 15 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#14 SMP PREEMPT Tue Apr 8 13:45:46 CDT 2014"
+#define UTS_VERSION "#15 SMP PREEMPT Tue Apr 8 17:56:13 CDT 2014"
 #define LINUX_COMPILE_BY "blackburn"
 #define LINUX_COMPILE_HOST "blackburn-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g3d6eae3-dirty"
+#define UTS_RELEASE "3.4.0PSYCOkernelR4-g3067da1-dirty"
