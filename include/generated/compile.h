32c01c1a3d455a12eb41e2b5248c51393f0ced66
Paul Reioux <reioux@gmail.com>
2014-04-05 16:47:04 -0700

    drivers/Makefile: add exFat support
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/Makefile
    
    Former-commit-id: 7c2c591a9979bdf906cd31055b6dca8adcbca86b

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Apr 3 18:41:12 CDT 2014"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Apr 6 20:58:06 CDT 2014"
 #define LINUX_COMPILE_BY "blackburn"
 #define LINUX_COMPILE_HOST "blackburn-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g4b0b84e-dirty"
+#define UTS_RELEASE "3.4.0-gf9a3861-dirty"
