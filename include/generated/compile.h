28bcb49651267df63a3ba5e7a561d2e6d40bab6f
Blackburn <blackburn2929@gmail.com>
2014-04-07 13:37:25 -0500

    Overclock SLIMBUS root freq for better audio quality (Flar2)
    Former-commit-id: 157e157ba859718034420871c47f271be65f2444

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Sun Apr 6 21:36:18 CDT 2014"
+#define UTS_VERSION "#9 SMP PREEMPT Mon Apr 7 13:26:04 CDT 2014"
 #define LINUX_COMPILE_BY "blackburn"
 #define LINUX_COMPILE_HOST "blackburn-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
